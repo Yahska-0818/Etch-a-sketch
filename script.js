16dd52687d28d76b7c62cc26b4371b792641d275
Yahska-0818 <101645835+Yahska-0818@users.noreply.github.com>
2025-05-06 16:48:27 +0000

    added different paint styles

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+function getRandomNumber(min, max) {
+    const minCeiled = Math.ceil(min);
+    const maxFloored = Math.floor(max);
+    return Math.floor(Math.random() * (maxFloored - minCeiled) + minCeiled)
+}
+
 const divContainer = document.querySelector(".container")
 const totalAreaAvail = 75*75
 
@@ -16,6 +22,8 @@ if (numOfGrid > 100) {
 areaPerEach = (totalAreaAvail)/ (numOfGrid*numOfGrid)
 dimensionPerEach = areaPerEach**(1/2)
 
+let currentMode = "clear";
+
 function setupGrid(gridSize,dimensions) {
     for (let i = 0; i < gridSize**2; i++) {
         let drawDiv = document.createElement("div")
@@ -24,7 +32,18 @@ function setupGrid(gridSize,dimensions) {
         drawDiv.style.backgroundColor = "#F9F5FF"
         divContainer.appendChild(drawDiv)
         drawDiv.addEventListener("mouseover", ()=> {
-            drawDiv.style.backgroundColor = "#15191D"
+            if (currentMode === "classic") {
+                drawDiv.style.backgroundColor = "#15191D"
+            }
+            else if (currentMode === "rainbow") {
+                let red = getRandomNumber(0,256)
+                let green = getRandomNumber(0,256)
+                let blue = getRandomNumber(0,256)
+                drawDiv.style.backgroundColor =  `rgb(${red},${green},${blue})`
+            }
+            else if (currentMode === "clear") {
+                drawDiv.style.backgroundColor = "#F9F5FF"
+            }
         })
     }
 }
