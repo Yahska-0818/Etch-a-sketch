45a5c0d77be8099423cc5348997953b4ae55723e
Yahska-0818 <101645835+Yahska-0818@users.noreply.github.com>
2025-05-06 16:28:46 +0000

    added functionality for clear and custom grid buttons

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
 const divContainer = document.querySelector(".container")
 const totalAreaAvail = 75*75
-let numOfGrid = prompt("Enter grid number (1-100)")
+
+let numOfGrid = parseInt(prompt("Enter grid number (1-100)"))
+if (numOfGrid > 100) {
+    numOfGrid = 100
+} else if (numOfGrid <= 0) {
+    alert("Enter a valid number")
+    location.reload()
+} else if (!(Number.isInteger(numOfGrid))) {
+    alert("Enter a valid number")
+    location.reload()
+}
+
+
 areaPerEach = (totalAreaAvail)/ (numOfGrid*numOfGrid)
 dimensionPerEach = areaPerEach**(1/2)
 
@@ -9,12 +21,27 @@ function setupGrid(gridSize,dimensions) {
         let drawDiv = document.createElement("div")
         drawDiv.classList.add("grid-pixel")
         drawDiv.style.cssText = `height:${dimensions}vh; width:${dimensions}vh`
-        drawDiv.style.backgroundColor = "black"
+        drawDiv.style.backgroundColor = "#F9F5FF"
         divContainer.appendChild(drawDiv)
         drawDiv.addEventListener("mouseover", ()=> {
-            drawDiv.style.backgroundColor = "white"
+            drawDiv.style.backgroundColor = "#15191D"
         })
     }
 }
+
+const reloadPage = document.getElementById("customGrid")
+
+reloadPage.addEventListener("click", ()=> {
+    location.reload()
+})
+
+const resetPage = document.getElementById("resetPage")
+
+resetPage.addEventListener("click", ()=> {
+    for (let i = 0; i < numOfGrid**2; i++) {
+        divContainer.innerHTML = ""
+    }
+    setupGrid(numOfGrid,dimensionPerEach)
+})
 
 setupGrid(numOfGrid,dimensionPerEach)
