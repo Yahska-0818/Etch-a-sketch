64cc961fe90c5447497789faa0cba7cbc2ff915b
Yahska-0818 <101645835+Yahska-0818@users.noreply.github.com>
2025-05-06 13:48:47 +0000

    implemented basic color change

--- script.js
+++ script.js
@@ -4,9 +4,17 @@ let numOfGrid = prompt("Enter grid number (1-100)")
 areaPerEach = (totalAreaAvail)/ (numOfGrid*numOfGrid)
 dimensionPerEach = areaPerEach**(1/2)
 
-for (let i = 0; i < numOfGrid*numOfGrid; i++) {
-    let drawDiv = document.createElement("div")
-    drawDiv.style.cssText = `height:${dimensionPerEach}vh; width:${dimensionPerEach}vh`
-    drawDiv.style.backgroundColor = "black"
-    divContainer.appendChild(drawDiv)
+function setupGrid(gridSize,dimensions) {
+    for (let i = 0; i < gridSize**2; i++) {
+        let drawDiv = document.createElement("div")
+        drawDiv.classList.add("grid-pixel")
+        drawDiv.style.cssText = `height:${dimensions}vh; width:${dimensions}vh`
+        drawDiv.style.backgroundColor = "black"
+        divContainer.appendChild(drawDiv)
+        drawDiv.addEventListener("mouseover", ()=> {
+            drawDiv.style.backgroundColor = "white"
+        })
+    }
 }
+
+setupGrid(numOfGrid,dimensionPerEach)
